622264017eb88b86630ebad1fe924875deedd46e
trang09041993 <tranght0904@gmail.com>
2017-11-17 21:36:18 +0700

    by Trang

--- ExamManager/src/com/bkhn/gui/GuiManageExam.java
+++ ExamManager/src/com/bkhn/gui/GuiManageExam.java
@@ -8,8 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 
-import com.bkhn.interfacecommon.ICommonGui;
-
+import com.bkhn.gui.interfacecommon.ICommonGui;
 
 public class GuiManageExam extends JFrame implements ICommonGui{
 
@@ -20,10 +19,9 @@ public class GuiManageExam extends JFrame implements ICommonGui{
 	private static final int WIDTH_FRAME = 700;
 	private static final int HEIGHT_FRAME = 500;
 	private JLabel lblManageExam;
-	private JLabel lblOpenExam;
 	private JScrollPane scrollPane;
 	private JTextPane txtPane;
-	private JButton btnOK;
+	private JButton btnUpdate;
 
 	
 	public GuiManageExam() {
@@ -42,29 +40,33 @@ public class GuiManageExam extends JFrame implements ICommonGui{
 
 	@Override
 	public void addComps() {
-		lblManageExam = new JLabel("Quản lý đề thi");
+		lblManageExam = new JLabel("Danh sách đề thi có sẵn");
 		lblManageExam.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblManageExam.setBounds(33, 24, 131, 33);
-		add(lblManageExam);
-		
-		lblOpenExam = new JLabel("Mở đề thi có sẵn");
-		lblOpenExam.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblOpenExam.setBounds(33, 71, 138, 33);
-		add(lblOpenExam);
+		lblManageExam.setBounds(216, 27, 220, 33);
+		getContentPane().add(lblManageExam);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(33, 117, 617, 235);
-		add(scrollPane);
+		scrollPane.setBounds(33, 71, 617, 235);
+		getContentPane().add(scrollPane);
+		
+		btnUpdate = new JButton("Chỉnh sửa đề thi");
+		btnUpdate.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnUpdate.setBounds(90, 335, 143, 33);
+		getContentPane().add(btnUpdate);
 		
 		txtPane = new JTextPane();
+		txtPane.setBounds(33, 71, 615, 233);
+		getContentPane().add(txtPane);
 		txtPane.setText("không biết set chỗ này ra sao.");
-		scrollPane.setViewportView(txtPane);
 		
-		btnOK = new JButton("OK");
-		btnOK.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnOK.setBounds(466, 376, 58, 43);
-		add(btnOK);
+		JButton btnCustom = new JButton("Đề thi tùy chỉnh");
+		btnCustom.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnCustom.setBounds(268, 335, 135, 33);
+		getContentPane().add(btnCustom);
+		
+		JButton btnAuto = new JButton("Tạo đề thi tự động");
+		btnAuto.setFont(new Font("Dialog", Font.BOLD, 13));
+		btnAuto.setBounds(438, 335, 149, 33);
+		getContentPane().add(btnAuto);
 	}
-		
-	
 }
