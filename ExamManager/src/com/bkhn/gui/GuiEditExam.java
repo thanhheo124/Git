6c5090ec10b3dc5e7b5edbd3626d380842e642da
trang09041993 <tranght0904@gmail.com>
2017-11-17 21:28:54 +0700

    by trang
    còn điểm chưa biết làm

--- ExamManager/src/com/bkhn/gui/GuiEditExam.java
+++ ExamManager/src/com/bkhn/gui/GuiEditExam.java
@@ -1,12 +1,19 @@
 package com.bkhn.gui;
 
+import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -14,8 +21,17 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.table.DefaultTableModel;
 
-import com.bkhn.interfacecommon.ICommonGui;
+import com.bkhn.gui.interfacecommon.ICommonGui;
+import com.bkhn.model.ChoiceQuestion;
+import com.bkhn.model.Question;
+import com.bkhn.model.QuizQuestion;
 
+import javax.swing.border.LineBorder;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
 
 public class GuiEditExam extends JFrame implements ICommonGui{
 
@@ -24,7 +40,6 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 	private JLabel lblListQuestion;
 	private JScrollPane scrollPane;
 	private JTable tableListQuestion;
-	private JTextPane txtPaneInformationQuestion;
 	private JButton btnChooseRandomQuestion;
 	private JLabel lblPointQuestion;
 	private JTextField txtPointQuestion;
@@ -33,10 +48,8 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 	private JRadioButton radioAll;
 	private JRadioButton radioMulti;
 	private JRadioButton radioEssay;
-	private JLabel lblListQuestionInExam;
 	private JScrollPane scrollPane_1;
 	private JTable tableListQuestionInExam;
-	private JLabel lblInformationExam;
 	private Component lblTime;
 	private JLabel lblYear;
 	private JLabel lblSeason;
@@ -52,10 +65,20 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 	private JTextPane txtPaneInformationExam;
 	private JButton btnSaveExam;
 	private JButton btnDeleteExam;
-	private JScrollPane scrollPane_2;
-	private JScrollPane scrollPane_3;
-	private JButton btnThayCuHi;
-	private JButton btnXaCuHi;
+	private JTextPane txtpnDDD;
+	private JButton btnXaCuHi ;
+	private ArrayList<ChoiceQuestion> listChoice;
+	private ArrayList<ChoiceQuestion> listChoiceExam;
+	private ArrayList<QuizQuestion> listQuiz;
+	private ArrayList<QuizQuestion> listQuizExam;
+	
+	private DefaultTableModel dtmQuestion;
+	private DefaultTableModel dtmQuestionExam;
+	private int indexDtmQuestion;
+	private int indexExamQuestion;
+	private Random rand; // Global variable
+	
+	
 
 	public GuiEditExam() {
 		init();
@@ -71,9 +94,116 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 		getContentPane().setLayout(null);
 
 	}
+	
+	public int random(int min,int max) {
+		Random r=new Random();
+		int radomNum=r.nextInt(Math.abs(max - min)+1);
+		if(min<max) radomNum +=min;
+		else radomNum +=max;
+		System.out.println(radomNum);
+		return radomNum;
+	}
+	
+	public boolean checkQuestion(ArrayList<ChoiceQuestion> listC,ArrayList<QuizQuestion> listQ,String content){
+		boolean check=true;
+		if(listC !=null){
+			for(ChoiceQuestion q:listC){
+				if(content.equals(q.getContent())) check=false;	
+			}
+		}
+		if(listQ !=null){
+			for(QuizQuestion q:listQ){
+				if(content.equals(q.getContent())) check=false;
+			}
+		}
+		if (check==true) return check;
+		else{
+			JOptionPane.showMessageDialog(null,"question exists in exam! Try again!");
+			return false;
+		}
+		
+	}
+	
+	public void loadListQuestion(ArrayList<ChoiceQuestion> listC,ArrayList<QuizQuestion> listQ, DefaultTableModel dtm){
+		dtm.setRowCount(0);
+		if(listC !=null){
+			for(ChoiceQuestion q:listC){
+			Vector<Object> vec=new Vector<Object>();
+			vec.add(q.getContent());
+			dtm.addRow(vec);
+			}
+		}
+		if(listQ !=null){
+			for(QuizQuestion q:listQ){
+			Vector<Object> vec=new Vector<Object>();
+			vec.add(q.getContent());
+			dtm.addRow(vec);
+			}
+		}
+	}
+	
+	public void loadContenQuestion(ChoiceQuestion questionC,QuizQuestion questionQ,JTextPane jtp){
+		if(questionC !=null){
+			jtp.setText(questionC.QuestionToString());
+		}
+		if(questionQ !=null){
+			jtp.setText(questionQ.QuestionToString());
+		}
+	}
+	
+	public void loadContenExam(ArrayList<ChoiceQuestion> listC,ArrayList<QuizQuestion> listQ,JTextPane jtp){
+		String s="";
+		int k=1;
+		if(listC !=null){
+			for(int i=0;i<listC.size();i++){
+				s +=k+". "+listC.get(i).getContent()+"\n";
+				for(int j=0;j<listC.get(i).getChoices().size();j++){
+					s += listC.get(i).getChoices().get(j)+"\n";
+				}
+				k++;
+			}
+		}
+		if(listQ !=null){
+			for(int i=0;i<listQ.size();i++){
+				s+= k+". "+listQ.get(i).getContent()+"\n";
+				k++;
+			}
+			
+		}
+		jtp.setText(s);
+	}
+	
 
 	@Override
 	public void addComps() {
+		listChoice=new ArrayList<ChoiceQuestion>();
+		listChoiceExam=new ArrayList<ChoiceQuestion>();
+		listQuiz=new ArrayList<QuizQuestion>();
+		listQuizExam=new ArrayList<QuizQuestion>();
+		
+		//create list question
+		
+		ChoiceQuestion choiceQuestion0 = new ChoiceQuestion( "Choise question 0", "IT101", 9, 8, null, null);
+		choiceQuestion0.addChoices("hanoi");
+		choiceQuestion0.addAnswers("hanoi");
+		choiceQuestion0.addChoices("vietnam");
+		ChoiceQuestion choiceQuestion1 = new ChoiceQuestion( "Choise question 1", "IT101", 8, 0, null, null);
+		choiceQuestion1.addChoices("hanoi");
+		choiceQuestion1.addAnswers("hanoi");
+		choiceQuestion1.addChoices("vietnam");
+		
+		listChoice.add(choiceQuestion0);
+		listChoice.add(choiceQuestion1);
+		
+		//create list question
+		QuizQuestion question1 = new QuizQuestion( "QuizQuestion 0", "IT101", 2, 3, "suggestion0");
+		QuizQuestion question2 = new QuizQuestion( "QuizQuestion 1", "IT101", 2, 3, "suggestion1");
+		QuizQuestion question3 = new QuizQuestion( "QuizQuestion 2", "IT101", 2, 3, "suggestion2");
+		 listQuiz.add(question1);
+		 listQuiz.add(question2);
+		 listQuiz.add(question3);
+		 
+		
 		lblListQuestion = new JLabel("Danh sách câu hỏi");
 		lblListQuestion.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblListQuestion.setBounds(12, 13, 148, 22);
@@ -84,16 +214,8 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 		getContentPane().add(lblPointQuestion);
 		
 		lblTypeQuestion = new JLabel("Dạng câu hỏi");
-		lblTypeQuestion.setBounds(22, 390, 116, 16);
+		lblTypeQuestion.setBounds(22, 46, 116, 16);
 		getContentPane().add(lblTypeQuestion);
-		
-		lblListQuestionInExam = new JLabel("Danh sách câu hỏi trong đề");
-		lblListQuestionInExam.setBounds(22, 449, 194, 16);
-		getContentPane().add(lblListQuestionInExam);
-		
-		lblInformationExam = new JLabel("Thông tin về đề thi");
-		lblInformationExam.setBounds(12, 731, 161, 16);
-		getContentPane().add(lblInformationExam);
 		
 		lblTime = new JLabel("Thời gian");
 		lblTime.setBounds(12, 760, 83, 16);
@@ -147,11 +269,79 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 		getContentPane().add(txtPointQuestion);
 		txtPointQuestion.setColumns(10);
 		
+		
+		txtPaneInformationExam = new JTextPane();
+		txtPaneInformationExam.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		txtPaneInformationExam.setBounds(454, 440, 396, 257);
+		getContentPane().add(txtPaneInformationExam);
+		
 		btnChooseRandomQuestion = new JButton("Chọn câu hỏi ngẫu nhiên");
-		btnChooseRandomQuestion.setBounds(161, 343, 220, 25);
+		btnChooseRandomQuestion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int sizeChoiec=listChoice.size();
+				int sizeQuiz=listQuiz.size();
+				
+				if(radioMulti.isSelected()){
+					indexDtmQuestion=random(0,sizeChoiec-1);
+					if(checkQuestion(listChoiceExam, null, listChoice.get(indexDtmQuestion).getContent()))
+						listChoiceExam.add(listChoice.get(indexDtmQuestion));
+				}
+				else if(radioEssay.isSelected()){
+					indexDtmQuestion=random(0,sizeQuiz-1);
+					if(checkQuestion(null, listQuizExam, listQuiz.get(indexDtmQuestion).getContent()))
+						listQuizExam.add(listQuiz.get(indexDtmQuestion));
+					
+				}
+				else if(radioAll.isSelected()){
+					indexDtmQuestion=random(0,sizeQuiz+sizeChoiec-1);
+					if(indexDtmQuestion<listChoice.size()){
+						
+						if(checkQuestion(listChoiceExam, null, listChoice.get(indexDtmQuestion).getContent()))
+							listChoiceExam.add(listChoice.get(indexDtmQuestion));
+					}
+					else{
+						int indexRealQuiz=indexDtmQuestion-listChoice.size();
+						if(checkQuestion(null, listQuizExam, listQuiz.get(indexRealQuiz).getContent()))
+							listQuizExam.add(listQuiz.get(indexRealQuiz));
+					}
+						
+				}
+				loadListQuestion(listChoiceExam, listQuizExam, dtmQuestionExam);
+				loadContenExam(listChoiceExam, listQuizExam, txtPaneInformationExam);
+				
+				
+			}
+		});
+		btnChooseRandomQuestion.setBounds(60, 361, 220, 25);
 		getContentPane().add(btnChooseRandomQuestion);
 		
 		btnAddToExam = new JButton("Thêm vào đề thi");
+		btnAddToExam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int indexRealQuiz=indexDtmQuestion-listChoice.size();
+				if(radioMulti.isSelected()){
+					if(checkQuestion(listChoiceExam, null, listChoice.get(indexDtmQuestion).getContent()))
+						listChoiceExam.add(listChoice.get(indexDtmQuestion));
+				}
+				else if(radioEssay.isSelected()){
+					if(checkQuestion(null, listQuizExam, listQuiz.get(indexDtmQuestion).getContent()))
+						listQuizExam.add(listQuiz.get(indexDtmQuestion));
+				}
+				else if(radioAll.isSelected()){
+					if(indexDtmQuestion<listChoice.size()){
+						if(checkQuestion(listChoiceExam, null, listChoice.get(indexDtmQuestion).getContent()))
+							listChoiceExam.add(listChoice.get(indexDtmQuestion));
+					}	
+					else{
+						if(checkQuestion(null, listQuizExam, listQuiz.get(indexRealQuiz).getContent()))
+							listQuizExam.add(listQuiz.get(indexRealQuiz));
+					}		
+				}
+				loadListQuestion(listChoiceExam, listQuizExam, dtmQuestionExam);
+				loadContenExam(listChoiceExam, listQuizExam, txtPaneInformationExam);
+			}
+		});
 		btnAddToExam.setBounds(684, 380, 148, 29);
 		getContentPane().add(btnAddToExam);
 		
@@ -168,15 +358,37 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 		getContentPane().add(btnDeleteExam);
 		
 		radioAll = new JRadioButton("Tất cả");
-		radioAll.setBounds(12, 415, 83, 25);
+		radioAll.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				radioMulti.setSelected(false);
+				radioEssay.setSelected(false);
+				loadListQuestion(listChoice, listQuiz, dtmQuestion);
+			}
+		});
+		radioAll.setBounds(32, 70, 83, 25);
 		getContentPane().add(radioAll);
 		
 		radioMulti = new JRadioButton("Trắc nghiệm");
-		radioMulti.setBounds(100, 415, 116, 25);
+		radioMulti.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				radioAll.setSelected(false);
+				radioEssay.setSelected(false);
+				loadListQuestion(listChoice,null, dtmQuestion);
+			}
+		});
+		radioMulti.setBounds(113, 69, 103, 25);
 		getContentPane().add(radioMulti);
 		
+		
 		radioEssay = new JRadioButton("Tự luận");
-		radioEssay.setBounds(220, 415, 127, 25);
+		radioEssay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				radioAll.setSelected(false);
+				radioMulti.setSelected(false);
+				loadListQuestion(null,listQuiz, dtmQuestion);
+			}
+		});
+		radioEssay.setBounds(231, 70, 127, 25);
 		getContentPane().add(radioEssay);
 		
 		radioSeason1 = new JRadioButton("1");
@@ -192,61 +404,148 @@ public class GuiEditExam extends JFrame implements ICommonGui{
 		getContentPane().add(rdSeasonSummner);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(22, 64, 359, 266);
+		scrollPane.setBounds(22, 118, 359, 226);
 		getContentPane().add(scrollPane);
 		
+		dtmQuestion=new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"List Questions"
+				}
+			);
 		tableListQuestion = new JTable();
-		tableListQuestion.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"New column"
-			}
-		));
-		tableListQuestion.setForeground(Color.WHITE);
+		tableListQuestion.setModel(dtmQuestion);
+		tableListQuestion.setForeground(Color.BLACK);
 		scrollPane.setViewportView(tableListQuestion);
 		
+		tableListQuestion.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				indexDtmQuestion=tableListQuestion.getSelectedRow();
+				if(radioMulti.isSelected()){
+					loadContenQuestion(listChoice.get(indexDtmQuestion),null, txtpnDDD);
+				}
+				else if(radioEssay.isSelected()){
+					loadContenQuestion(null,listQuiz.get(indexDtmQuestion),txtpnDDD);
+				}
+				else if(radioAll.isSelected()){
+					int indexRealQuiz=indexDtmQuestion-listChoice.size();
+					if(indexDtmQuestion<listChoice.size())
+						loadContenQuestion(listChoice.get(indexDtmQuestion),null, txtpnDDD);
+					else
+						loadContenQuestion(null,listQuiz.get(indexRealQuiz),txtpnDDD);
+						
+				}
+				
+			}
+			
+		});
+		
+		
 		scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(16, 490, 365, 172);
+		scrollPane_1.setBounds(16, 440, 365, 172);
 		getContentPane().add(scrollPane_1);
 		
+		dtmQuestionExam=new DefaultTableModel(
+				new Object[][] {
+					{null},
+				},
+				new String[] {
+					"List Question in Exam"
+				}
+			);
 		tableListQuestionInExam = new JTable();
-		tableListQuestionInExam.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null},
-			},
-			new String[] {
-				"New column"
-			}
-		));
+		tableListQuestionInExam.setModel(dtmQuestionExam);
 		scrollPane_1.setViewportView(tableListQuestionInExam);
 		
-		scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(433, 33, 440, 340);
-		getContentPane().add(scrollPane_2);
+		tableListQuestionInExam.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				indexExamQuestion=tableListQuestionInExam.getSelectedRow();					
+				if(indexExamQuestion<listChoiceExam.size())
+				{
+					loadContenQuestion(listChoiceExam.get(indexExamQuestion),null, txtpnDDD);
+					loadListQuestion(listChoice,null, dtmQuestion);
+					radioAll.setSelected(false);
+					radioEssay.setSelected(false);
+					radioMulti.setSelected(true);
+					
+				}
+				else
+				{
+					int indexRealQuiz=indexExamQuestion-listChoiceExam.size();
+					loadContenQuestion(null,listQuizExam.get(indexRealQuiz),txtpnDDD);
+					loadListQuestion(null,listQuiz, dtmQuestion);
+					radioAll.setSelected(false);
+					radioMulti.setSelected(false);
+					radioEssay.setSelected(true);
+				}
+			}
+		});
 		
-		txtPaneInformationQuestion = new JTextPane();
-		scrollPane_2.setViewportView(txtPaneInformationQuestion);
-		
-		scrollPane_3 = new JScrollPane();
-		scrollPane_3.setBounds(444, 484, 427, 321);
-		getContentPane().add(scrollPane_3);
-		
-		txtPaneInformationExam = new JTextPane();
-		scrollPane_3.setViewportView(txtPaneInformationExam);
-		
-		btnThayCuHi = new JButton("Thay câu hỏi");
-		btnThayCuHi.setForeground(Color.WHITE);
-		btnThayCuHi.setBackground(Color.BLUE);
-		btnThayCuHi.setBounds(17, 678, 121, 40);
+		JButton btnThayCuHi = new JButton("Thay câu hỏi");
+		btnThayCuHi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String value=tableListQuestion.getValueAt(indexDtmQuestion,0).toString();
+			
+				if(indexExamQuestion<listChoiceExam.size())
+				{
+					
+					loadContenQuestion(listChoice.get(indexDtmQuestion),null, txtpnDDD);
+
+					if(checkQuestion(listChoiceExam,null, value)){
+						int reply = JOptionPane.showConfirmDialog(null,"Do you really change?","", JOptionPane.YES_NO_OPTION);
+						if (reply == JOptionPane.YES_OPTION) {
+							listChoiceExam.remove(indexExamQuestion);
+							listChoiceExam.add(listChoice.get(indexDtmQuestion));
+							
+				}	}	}
+				else{
+					loadContenQuestion(null,listQuiz.get(indexDtmQuestion),txtpnDDD);
+					if(checkQuestion(null,listQuizExam, value)){
+						int reply = JOptionPane.showConfirmDialog(null,"Do you really change?","", JOptionPane.YES_NO_OPTION);
+						if (reply == JOptionPane.YES_OPTION) {
+							listQuizExam.remove(indexExamQuestion-listChoiceExam.size());
+							listQuizExam.add(listQuiz.get(indexDtmQuestion));
+			}}	}
+				loadListQuestion(listChoiceExam, listQuizExam, dtmQuestionExam);
+				loadContenExam(listChoiceExam, listQuizExam, txtPaneInformationExam);
+				
+			}
+		});
+		btnThayCuHi.setBounds(66, 657, 116, 23);
 		getContentPane().add(btnThayCuHi);
 		
 		btnXaCuHi = new JButton("Xóa câu hỏi");
-		btnXaCuHi.setForeground(Color.WHITE);
-		btnXaCuHi.setBackground(Color.RED);
-		btnXaCuHi.setBounds(260, 675, 121, 40);
+		btnXaCuHi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(indexExamQuestion<listChoiceExam.size())
+				{
+					listChoiceExam.remove(indexExamQuestion);
+					
+				}
+				else
+				{
+					int indexRealQuiz=indexExamQuestion-listChoiceExam.size();
+					listQuizExam.remove(indexRealQuiz);
+				}
+				loadListQuestion(listChoiceExam, listQuizExam, dtmQuestionExam);
+				loadContenExam(listChoiceExam, listQuizExam, txtPaneInformationExam);
+			}
+		});
+		btnXaCuHi.setBounds(218, 657, 89, 23);
 		getContentPane().add(btnXaCuHi);
 		
+		txtpnDDD = new JTextPane();
+		txtpnDDD.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtpnDDD.setText("d\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd\r\nd");
+		txtpnDDD.setBounds(460, 118, 330, 226);
+		getContentPane().add(txtpnDDD);
+		
+		
 	}
-
 }
