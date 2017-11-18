eee1c73bc5baa09ac5abee1df14895029ab4fef8
manhlv <levanmanhbka@gmail.com>
2017-11-18 20:14:44 +0700

    delete controllerQuestion

--- ExamManager/src/main/Main.java
+++ ExamManager/src/main/Main.java
@@ -1,16 +1,13 @@
 package main;
 
 import com.bkhn.controller.Controller;
-import com.bkhn.controller.ControllerQuestion;
-import com.bkhn.gui.GuiCreatExamAuto;
-import com.bkhn.gui.GuiManager;
 
 public class Main {
 	public static void main(String[] args) {
 		Controller controller = new Controller();
 //		GuiCreatExamAuto gui = new GuiCreatExamAuto();
 //		gui.setVisible(true);
-//		ControllerQuestion controllerQuestion = new ControllerQuestion();
+//		ControllerQuestion controllerQuestion = new 	ControllerQuestion();
 //		GuiManager guiManager = new GuiManager();
 	}
 }
